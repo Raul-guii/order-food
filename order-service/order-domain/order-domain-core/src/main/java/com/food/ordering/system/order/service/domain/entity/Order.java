a2a33908c28cfc16ac31b0298e0d4ce8603c5bce
Raul-guii <raulawp460@gmail.com>
2026-08-26 09:30:42 -0300

    feat: implement order state transitions and add Customer/Restaurant entities

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
@@ -73,6 +73,45 @@ public class Order extends AggregateRoot<OrderId> {
         }
     }
 
+    private void updateFailureMessages(List<String> failureMessages) {
+        if (this.failureMessages != null && failureMessages != null){
+            this.failureMessages.addAll(failureMessages.stream().filter(message -> !message.isEmpty()).toList());
+        }
+        if (this.failureMessages == null){
+            this.failureMessages = failureMessages;
+        }
+    }
+
+    public void pay(){
+        if (orderStatus != OrderStatus.PENDING){
+            throw new DomainExpcetion("Order is not in correct state for payment operation!");
+        }
+        orderStatus = OrderStatus.PAID;
+    }
+
+    public void approve(){
+        if (orderStatus != OrderStatus.PAID){
+            throw new DomainExpcetion("Order is not in correct state for approve operation!");
+        }
+        orderStatus = OrderStatus.APPROVED;
+    }
+
+    public void initCancel(List<String> failureMessages){
+        if (orderStatus != OrderStatus.PAID){
+            throw new DomainExpcetion("Order is not in correct state for initCancel operation!");
+        }
+        orderStatus = OrderStatus.CANCELLING;
+        updateFailureMessages(failureMessages);
+    }
+
+    public void cancel(List<String> failureMessages){
+        if (!(orderStatus == OrderStatus.CANCELLING || orderStatus == OrderStatus.PENDING)){
+            throw new DomainExpcetion("Order is not in correct state for cancel operation!");
+        }
+        orderStatus = OrderStatus.CANCELLED;
+        updateFailureMessages(failureMessages);
+    }
+
     private Order(Builder builder) {
         super.setId(builder.orderId);
         customerId = builder.customerId;
