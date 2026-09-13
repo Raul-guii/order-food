299bee20422b427a958a76f5df9b3dbcf8ef2095
Raul-guii <raulawp460@gmail.com>
2026-09-13 03:52:02 -0300

    Fix OrderRepositoryImpl and Implement customer and restaurant data access: entities, repositories, mappers and adapters

--- order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
@@ -1,12 +1,10 @@
 package com.food.ordering.system.order.service.dataaccess.order.adapter;
 
-import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.dataaccess.order.mapper.OrderDataAccessMapper;
 import com.food.ordering.system.order.service.dataaccess.order.repository.OrderJpaRepository;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
 import com.food.ordering.system.order.service.domain.valueObject.TrackingId;
-import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
